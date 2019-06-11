871efdb7c98a4e474138c30bac0c45b81a51801d
EC2 Default User <ec2-user@ip-172-31-20-23.eu-west-1.compute.internal>
2019-06-11 16:51:16 +0000

    Fix mongo connection

---
+++ samples/basic_sample/consumer/db.js
@@ -0,0 +1,52 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var logger = require('../../util/logger');
+var util = require('util');
+
+function DB() {
+  this.db = "empty";
+  this.log = logger().getLogger('mongoMange-DB');
+}
+
+DB.prototype.connect = function (uri, callback) {
+  this.log.info(util.format('About to connect to DB'));
+  if (this.db != "empty") {
+    callback();
+    this.log.info('Already connected to database.');
+  } else {
+    var _this = this;
+    MongoClient.connect(uri, function (err, client) {
+      if (err) {
+        _this.log.info(util.format('Error connecting to DB: %s', err.message));
+        callback(err);
+      } else {
+        _this.db = client.db('scorepad-staging');
+        _this.log.info(util.format('Connected to database.'));
+        callback();
+      }
+    })
+  }
+}
+
+DB.prototype.close = function (callback) {
+  log.info('Closing database');
+  this.db.close();
+  this.log.info('Closed database');
+  callback();
+}
+
+DB.prototype.addDocument = function (coll, doc, callback) {
+  var collection = this.db.collection(coll);
+  var _this = this;
+  collection.insertOne(doc, function (err, result) {
+    if (err) {
+      _this.log.info(util.format('Error inserting document: %s', err.message));
+      callback(err.message);
+    } else {
+      _this.log.info(util.format('Inserted document into %s collection.', coll));
+      callback();
+    }
+  });
+};
+
+module.exports = DB;
---
+++ samples/basic_sample/consumer/logging_consumer_app.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+var kcl = require('../../..');
+var logger = require('../../util/logger');
+var DB = require('./db.js')
+
+var mongodbConnectString = process.env.MONGO_URL
+var mongodbCollection = 'logdata'
+var database = new DB;
+
+function recordProcessor() {
+  var log = logger().getLogger('recordProcessor');
+  var shardId;
+
+  return {
+
+    initialize: function (initializeInput, completeCallback) {
+      shardId = initializeInput.shardId;
+
+      // WARNING – the connection string may contain the password and so consider removing logging for any production system
+      log.info(util.format('About to connect to %s.', mongodbConnectString));
+      database.connect(mongodbConnectString, function (err) {
+        log.info(util.format('Back from connecting to %s', mongodbConnectString));
+        if (err) {
+          log.info(util.format('Back from connecting to %s', mongodbConnectString));
+        }
+        completeCallback();
+      })
+    },
+
+    processRecords: function (processRecordsInput, completeCallback) {
+      log.info(util.format('In processRecords', mongodbConnectString));
+
+      if (!processRecordsInput || !processRecordsInput.records) {
+        completeCallback();
+        return;
+      }
+      var records = processRecordsInput.records;
+      var record, data, sequenceNumber, partitionKey, objectToStore;
+      for (var i = 0; i < records.length; ++i) {
+        record = records[i];
+        data = new Buffer(record.data, 'base64').toString();
+        sequenceNumber = record.sequenceNumber;
+        partitionKey = record.partitionKey;
+        log.info(util.format('ShardID: %s, Record: %s, SeqenceNumber: %s, PartitionKey:%s', shardId, data, sequenceNumber, partitionKey));
+        objectToStore = {};
+        try {
+          objectToStore = JSON.parse(data);
+        }
+        catch (err) {
+          // Looks like it wasn't JSON so store the raw string
+          objectToStore.payload = data;
+        }
+        objectToStore.metaData = {};
+        objectToStore.metaData.mongoLabel = "Added by MongoMange";
+        objectToStore.metaData.timeAdded = new Date();
+        database.addDocument(mongodbCollection, objectToStore, function (err) { })
+      }
+      if (!sequenceNumber) {
+        completeCallback();
+        return;
+      }
+      // If checkpointing, completeCallback should only be called once checkpoint is complete.
+      processRecordsInput.checkpointer.checkpoint(sequenceNumber, function (err, sequenceNumber) {
+        log.info(util.format('Checkpoint successful. ShardID: %s, SeqenceNumber: %s', shardId, sequenceNumber));
+        completeCallback();
+      });
+    },
+
+    shutdown: function (shutdownInput, completeCallback) {
+      // Checkpoint should only be performed when shutdown reason is TERMINATE.
+      if (shutdownInput.reason !== 'TERMINATE') {
+        completeCallback();
+        return;
+      }
+      // Whenever checkpointing, completeCallback should only be invoked once checkpoint is complete.
+      database.close(function () {
+        shutdownInput.checkpointer.checkpoint(function (err) {
+          completeCallback();
+        });
+      });
+    }
+  };
+}
+
+kcl(recordProcessor()).run();
